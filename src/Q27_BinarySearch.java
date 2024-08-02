0d22de60263721ccd07fab87896a5dda48304a3a
XOOO <chapant@gmail.com>
2024-08-02 14:33:46 -0400

    Q27_BinarySearch finished.

--- src/Q27_BinarySearch.java
+++ src/Q27_BinarySearch.java
@@ -1,5 +1,23 @@
 public class Q27_BinarySearch {
     public int search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
 
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[mid] < target) i = mid + 1;
+            else j = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Q27_BinarySearch obj = new Q27_BinarySearch();
+        int[] nums = {1, 3, 5, 7, 9, 11, 13, 15};
+
+        System.out.println(obj.search(nums, 11));
     }
 }
